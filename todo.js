3842f4cc76591b36950c2eb191b232e9f6591de6
algejo01 <jalger001@gmail.com>
2015-10-19 12:20:15 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,9 +1,14 @@
 newitem=function(){
       ulist= document.querySelector("#list1");
       item=document.createElement("li");
+      box = document.createElement("input");
+      box.type="checkbox";
       text= document.querySelector("#tbox").value;
+      prio = document.querySelector("#priority").value;
       li=document.createTextNode(text);
+      item.className="prio";
       item.appendChild(li);
+      ulist.appendChild(box);
       ulist.appendChild(item);
       
       
