d1a3c4b4fd8f1c6968c9c34fd550c88b7aedc8de
algejo01 <jalger001@gmail.com>
2015-10-19 12:09:38 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,10 +2,8 @@ newitem=function(){
       ulist= document.querySelector("#list1")
       item=document.createElement("li");
       text= document.querySelector("#tbox").value;
-      box = document.querySelector("cbox").type;
       li=document.createTextNode(text);
       item.appendChild(li);
-      ulist.appendChild(box);
       ulist.appendChild(item);
       
       
